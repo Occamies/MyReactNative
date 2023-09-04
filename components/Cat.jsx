b7754896cdbb769375ad72b77d9e96ca451a35f7
Occamies <lasseknudsen01@hotmail.com>
2023-09-04 13:57:53 +0200

    reactApp

---
+++ components/Cat.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text, View} from 'react-native';
+
+
+const Cat = () => {
+  return (
+    <View>
+      <Text>Hello, I am CAT!</Text>
+    </View>
+  );
+}
+
+export default Cat
